38d48127c0cf6b471872027046df873d5e954dde
ArthurVolkov <bafequan@gmail.com>
2019-03-17 20:38:11 +0200

    duplicate kode in file

--- multiplication_table_test.py
+++ multiplication_table_test.py
@@ -3,3 +3,8 @@ for row in range(1, max_ + 1):
     for column in range(1, max_ + 1):
         print(row * column, end='\t')
     print()
+max_ = 14
+for row in range(1, max_ + 1):
+    for column in range(1, max_ + 1):
+        print(row * column, end='\t')
+    print()
